abc12d423b7c62a38416111902fe1f6e1bef6b02
YM202c2100 <y.m202c2100@gmail.com>
2024-09-02 15:27:23 +0900

    フォームと合わせたdb処理

---
+++ database/DbSearchProduct.php
@@ -0,0 +1,38 @@
+<form action=<?= $_SERVER["REQUEST_URI"]?> method="get">
+  <input type="number" 
+    name="product_id" 
+    placeholder="検索したい商品IDを入力…"
+  >
+  <button type="submit">検索</button>
+</form>
+
+<?php
+  if(!isset($_GET["product_id"])) die();
+  
+  $message = "";
+  try{
+    $dsn = 'mysql:host=localhost;port=8889;dbname=test_phpdb';
+    $conn = new PDO($dsn, 'test_user', 'pwd');
+
+    $pst = $conn->prepare(
+      "SELECT `name`, `delete_flg` from mst_products
+       where id = :id"
+    );
+    $pst->bindValue(':id', $_GET['product_id'], PDO::PARAM_INT);
+
+    $pst->execute();
+    $result = $pst->fetch(PDO::FETCH_ASSOC);
+
+    if(empty($result)
+       ||$result["delete_flg"] === 1){
+      $message = "一致する商品が見つかりません。";
+    }else{
+      $message = "商品名は{$result["name"]}です。";
+    }
+
+  }catch(Exception){
+    $message = "時間をおいて再度お試しください";
+  }
+
+  echo $message;
+?>
