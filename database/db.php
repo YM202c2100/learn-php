7eb33a007fdf028a63e1c80ae0a88941084eb64a
YM202c2100 <y.m202c2100@gmail.com>
2024-09-02 13:14:31 +0900

    テーブルに対する変更操作の練習

--- database/db.php
+++ database/db.php
@@ -1,9 +1,63 @@
 <?php
-$dns = 'mysql:dbname=test_db;host=localhost;port=8889';
-$conn = new PDO($dns, 'root', 'root');
+$dns = 'mysql:dbname=test_phpdb;host=localhost;port=8889';
+$conn = new PDO($dns, 'test_user', 'pwd');
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$pst = $conn->query("SELECT * FROM mst_prefs");
+function disp_assoc($pst){
+  echo '<pre>';
+  print_r($pst->fetchAll());
+  echo '</pre>';
+}
 
-echo '<pre>';
-print_r($pst->fetchAll(PDO::FETCH_ASSOC));
-echo '</pre>';
+try{
+  // 変更前のレコード確認
+  $stmt = $conn->query('SELECT * from txn_stocks');
+  disp_assoc($stmt);
+  
+  // 店舗Cのすべての在庫数を+10
+  $conn->exec("UPDATE txn_stocks as stock set `amount` = `amount`+10 where stock.shop_id = 3");
+
+  echo "has changed amount";
+  // 変更後のレコード確認
+  $stmt = $conn->query('SELECT * from txn_stocks');
+  disp_assoc($stmt);
+}catch(Error $error){
+  echo "error発生:{$error}";
+}
+
+
+
+try {
+  $stmt = $conn->query(
+    "SELECT `name`, `pref_id` from mst_shops"
+  );
+
+  disp_assoc($stmt);
+
+  $ok = $conn->exec(
+    "INSERT into mst_shops (`name`, `pref_id`, `updated_by`)
+     values ('店舗D', 4, 'me')"
+  );
+
+  echo "has changed";
+  $stmt = $conn->query(
+    "SELECT `name`, `pref_id` from mst_shops"
+  );
+  disp_assoc($stmt);
+
+} catch (Error $e) {
+  echo "エラー";
+  throw "エラー発生:{$e}";
+}
+
+try {
+  $amoutChairShopA = $conn->query(
+    "SELECT amount from txn_stocks
+     where shop_id=1 and product_id=2"
+  );
+  disp_assoc($amoutChairShopA);
+
+} catch (Exception $error) {
+  throw $error;
+}
